65b039a8aa8d405cf5185ea63a03b3eb1666c681
makoragi <makoragi@gmail.com>
2022-02-27 17:29:30 +0900

    勝者を判定して表示

--- src/index.js
+++ src/index.js
@@ -44,9 +44,15 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
   
-      return (
+        return (
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
@@ -65,7 +71,7 @@ function Square(props) {
             {this.renderSquare(8)}
           </div>
         </div>
-      );
+        );
     }
   }
   
@@ -85,6 +91,27 @@ function Square(props) {
     }
   }
   
+  //勝者である X か O、もしくは null を返す
+  function calculateWinner(squares) {
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i=0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+              return squares[a];
+          }
+      }
+      return null;
+  }
+
   // ========================================
   
   ReactDOM.render(
