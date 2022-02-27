fb368211e4f15e4796d3619abf4b1d0a3e6a9a1e
makoragi <makoragi@gmail.com>
2022-02-27 17:22:24 +0900

    XとOを交互に置けるようにする

--- src/index.js
+++ src/index.js
@@ -20,14 +20,18 @@ function Square(props) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true, //先手はXにする
         }
     }
 
     handleClick(i) {
         // ミューテートを伴わないデータの変更
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -40,7 +44,7 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
