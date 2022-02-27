5ee1c642258c1a10f663523c7962e115365fa0de
makoragi <makoragi@gmail.com>
2022-02-27 17:48:27 +0900

    履歴を保存できるようにする

--- src/index.js
+++ src/index.js
@@ -16,49 +16,18 @@ function Square(props) {
   }
   
   class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true, //先手はXにする
-        }
-    }
-
-    handleClick(i) {
-        // ミューテートを伴わないデータの変更
-        const squares = this.state.squares.slice();
-        // 勝者が決まっていたり、すでに埋めているマスなら何もしない
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
     renderSquare(i) {
         return(
             <Square
-                value={this.state.squares[i]} //Boardで管理している配列から値を渡す
-                onClick={() => this.handleClick(i)} //クリックされたときこれ叩けよと言う指示
+                value={this.props.squares[i]} 
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
   
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
-  
         return (
         <div>
-          <div className="status">{status}</div>
           <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
@@ -80,14 +49,55 @@ function Square(props) {
   }
   
   class Game extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        // 履歴 (Gameに置いてBoardから取り除く)
+        history: [{
+          squares: Array(9).fill(null)
+        }],
+        xIsNext: true,
+      };
+    }
+
+    handleClick(i) {
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      // ミューテートを伴わないデータの変更
+      const squares = current.squares.slice();
+      // 勝者が決まっていたり、すでに埋めているマスなら何もしない
+      if (calculateWinner(squares) || squares[i]) {
+          return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+          history: history.concat([{
+            squares: squares
+          }]),
+          xIsNext: !this.state.xIsNext,
+      });
+    }
+
     render() {
+      const history = this.state.history;
+      const current = history[history.length - 1];
+      const winner = calculateWinner(current.squares);
+      let status;
+      if (winner) {
+        status = 'Winner: ' + winner;
+      } else {
+        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+      }
       return (
         <div className="game">
           <div className="game-board">
-            <Board />
+            <Board 
+              squares={current.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
+            <div>{status}</div>
             <ol>{/* TODO */}</ol>
           </div>
         </div>
